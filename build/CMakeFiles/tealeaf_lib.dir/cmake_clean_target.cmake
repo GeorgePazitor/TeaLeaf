0f94804a298d2a78a188c7b461129c47635522d0
George Pazitor <george@localhost.localdomain>
2026-02-15 14:17:20 +0100

    merged with optimized version plus conjugate gradient implem

--- build/CMakeFiles/tealeaf_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libtealeaf_lib.a"
-)
--- build/CMakeFiles/tealeaf_lib.dir/src/kernels/field_summary_kernel.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/tealeaf_lib.dir/src/kernels/field_summary_kernel.cpp.o: \
- /home/george/Code/TeaLeaf/src/kernels/field_summary_kernel.cpp \
- /usr/include/stdc-predef.h \
- /home/george/Code/TeaLeaf/src/include/kernels/field_summary_kernel.h \
- /usr/lib/gcc/x86_64-linux-gnu/12/include/omp.h
